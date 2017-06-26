e506505500a6367511784f249805c25e9802379a
Selin Ovali <sovali@purdue.edu>
2017-06-26 14:29:41 -0400

    added age & color input and a text that changes color (in JS)

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function updateHeading(ev) {
     ev.preventDefault();
     const f = ev.target
     const name = f.personName.value
+    const age = f.personAge.value
+    const color = f.personColor.value
     const heading = document.querySelector('h1')
+    const colorheader = document.querySelector('#colorDisplay')
     heading.textContent = name
+    const change = document.querySelector('#inputDisplay')
+    change.innerHTML = "Name: " + name + " Age: " + age;
+    colorDisplay.innerHTML = "I like " + color + " too!";
+    colorDisplay.style.color = color;
     //heading.innerHTML = name
 }
 
